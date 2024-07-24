ee8045763665592e0df545733b84213ed2055c99
unknown <ericryanbowser@gmail.com>
2024-07-23 21:44:49 -0600

    just addy

--- src/index.js
+++ src/index.js
@@ -32,11 +32,11 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root') )
     .render(
-        <div className={'container-lg text-3xl font-sans items-center text-center justify-normal bg-black text-white p-44'}>
-            <h3>my name is <span className={'text-blue-400'}>Bunker</span></h3>
-            <h3>my owner is <span className={'text-red-400'}>Eric Bowser</span></h3>
+        <div className={'flex flex-col text-3xl font-sans items-center text-center text-white bg-black justify-center min-h-screen'}>
+            <h3>My name is <span className={'text-blue-400'}>Bunker</span></h3>
+            <h3>My owner is <span className={'text-red-400'}>Eric Bowser</span></h3>
             <h3 className={'text-orange-400'}>5154 S 5200 W</h3>
-            <h3>Kearns, UT, 84118</h3>
+            <h3 className={'text-purple-500'}>Kearns, UT, 84118</h3>
             <h3 className={'text-green-400 bolder'}>(435) 494 - 8030</h3>
         </div>
 /*
